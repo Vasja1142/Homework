12b864fbf343de7519943145c2f05f3049af0aed
Vasiliy Gordeev <vasja11420@gmail.com>
2023-01-30 17:07:09 +0500

    Выполнил 2 задание

---
+++ Homework002/Task002/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+int x = 100;
+
+if (num / 100 > 0)
+{
+    while (num / x > 10)
+    {
+       x = x * 10; 
+    }
+    
+    x = x / 10;
+    int y = num % x;
+    int result = y / (x / 10);
+    Console.Write(result);
+}
+else
+{
+    Console.Write("В данном числе нет третьей цифры");
+}
