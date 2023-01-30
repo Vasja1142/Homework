54de0ef881c639f511aaa84e18c2d942e9d16756
Vasiliy Gordeev <vasja11420@gmail.com>
2023-01-30 16:40:29 +0500

    Выполнил 1 задание

---
+++ Homework002/Task001/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход 
+//трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num / 100 > 0 && num / 100 < 10)
+{
+    int hundreds = num / 100;
+    int result = (num - (hundreds * 100) - (num % 10)) / 10;
+    Console.Write(result);
+}
+else if (-num / 100 > 0 && -num / 100 < 10)
+{
+    int hundreds = -num / 100;
+    int result = (-num - (hundreds * 100) - (-num % 10)) / 10;
+    Console.Write(result);
+}
+else
+{
+    Console.Write("Данное число не является трехзначным");
+}
