04e9f2f2737900a5d53b7ca008589516dc40af9b
Vasya comp <vasja11420@gmail.com>
2023-02-06 21:19:13 +0500

    Изменения

---
+++ Homework003/Task003/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+
+Console.Write("Введите любое число: ");
+int num = int.Parse(Console.ReadLine()!);
+if (num > 0)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        double result = Math.Pow(count, 3);
+        Console.WriteLine($"{count}^3 = {result}");
+        count++;
+    }
+}
+else
+{
+    Console.Write("Я не умею работать с отрицательными числами");
+}
