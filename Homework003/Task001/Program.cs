2c42ad46e90856a79cdaea8c40c01d3c198a3215
Vasya comp <vasja11420@gmail.com>
2023-02-06 21:18:05 +0500

    Д/з - 4

---
+++ Homework003/Task001/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Clear();
+
+Console.Write("Введите пятизначное число: ");
+int ABCDE = int.Parse(Console.ReadLine()!);
+
+ABCDE = Math.Abs(ABCDE);
+
+if (ABCDE >= 10000 && ABCDE < 100000)
+{
+    int A = ABCDE / 10000;
+    int B = (ABCDE - A * 10000) / 1000;
+    int DE = ABCDE % 100;
+    if (B * 10 + A == DE)
+    {
+        Console.Write("Данное число является палиндропом");
+    }
+    else
+    {
+        Console.Write("Данное число не является палиндропом");
+    }
+}
+else
+{
+    Console.Write("Данное число не является пятизначным");
+}
---
+++ Homework003/Task002/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.Clear();
+
+Console.Write("Введите координату первой точки по оси X: ");
+int X1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату первой точки по оси Y: ");
+int Y1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату первой точки по оси Z: ");
+int Z1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату второй точки по оси X: ");
+int X2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату второй точки по оси Y: ");
+int Y2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату второй точки по оси Z: ");
+int Z2 = int.Parse(Console.ReadLine()!);
+
+int X = X1 - X2;
+int Y = Y1 - Y2;
+int Z = Z1 - Z2;
+double result = Math.Sqrt(X * X + Y * Y + Z * Z);
+
+Console.Write($"Расстояние между двумя точками: {result:f2}");
