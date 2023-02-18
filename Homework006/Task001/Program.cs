30350602a969b81e2d2b93255497145c72bd8820
Vasya comp <vasja11420@gmail.com>
2023-02-18 13:56:21 +0500

    Дз №6

---
+++ Homework006/Task001/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] CreateArray()
+{
+    Console.Write("Введите длинну массива: ");
+    int size = int.Parse(Console.ReadLine()!);
+    int[] array = new int[size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {i + 1}-e значение массива: ");
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+    return array;
+}
+
+int Counter(int[] array)
+{
+    int count = 0;
+    foreach (int element in array)
+    {
+        if(element > 0) count++;
+    }
+    return count;
+}
+
+int[] array = CreateArray();
+int result = Counter(array);
+
+Console.Clear();
+Console.WriteLine($"Массив: [{String.Join(", ", array)}]");
+Console.WriteLine($"Чисел больше 0: {result}");
---
+++ Homework006/Task002/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Даны 2 уравнения:");
+Console.WriteLine("y = k1 * x + b1");
+Console.WriteLine("y = k2 * x + b2");
+
+Console.Write("Введите переменную k1: ");
+double k1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите переменную b1: ");
+double b1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите переменную k2: ");
+double k2 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите переменную b2: ");
+double b2 = int.Parse(Console.ReadLine()!);
+
+double x;
+double y;
+
+// k1 * x + b1 = k2 * x + b2
+// x * k1 = k2 * x + b2 - b1
+// x * k1 - (k2 * x) = b2 - b1
+// x * (k1 - k2) = b2 - b1
+x = (b2 - b1) / (k1 - k2);
+y = k1 * x + b1;
+
+Console.WriteLine($"x = {x}");
+Console.WriteLine($"y = {y}");
